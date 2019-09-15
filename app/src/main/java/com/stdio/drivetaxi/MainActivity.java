f11818c70ccf44d63643f8756ff5a63927938265
Stdio <kwork-stdio@mail.ru>
2019-09-15 18:13:24 +0300

    0.0.1

--- app/src/main/java/com/stdio/drivetaxi/MainActivity.java
+++ app/src/main/java/com/stdio/drivetaxi/MainActivity.java
@@ -1,14 +1,95 @@
 package com.stdio.drivetaxi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.webkit.CookieManager;
+import android.webkit.WebView;
+import android.widget.ProgressBar;
+
+import static com.stdio.drivetaxi.WebViewHelper.afterChosePic;
+import static com.stdio.drivetaxi.WebViewHelper.cameraUri;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
+    public static final int REQUEST_CAMERA = 1;
+    public static final int REQUEST_CHOOSE = 2;
+    public static WebView mWebView;
+    public static ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        progressBar = findViewById(R.id.progressBar);
+        initView();
+    }
+
+    private void initView() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            if (0 != (getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE))
+            { WebView.setWebContentsDebuggingEnabled(true); }
+        }
+
+        CookieManager.getInstance().setAcceptCookie(true);
+        mWebView = (WebView) findViewById(R.id.maim_web);
+        mWebView.setWebViewClient(new MyWebViewClient());
+        mWebView.setWebChromeClient(new MyWebChromeClient(this, MainActivity.this));
+
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.getSettings().setDomStorageEnabled(true);
+        mWebView.getSettings().setLoadWithOverviewMode(true);
+        mWebView.getSettings().setUseWideViewPort(true);
+
+        mWebView.getSettings().setSupportZoom(true);
+        mWebView.getSettings().setBuiltInZoomControls(true);
+        mWebView.getSettings().setDisplayZoomControls(false);
+
+        mWebView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+        mWebView.setScrollbarFadingEnabled(false);
+        mWebView.loadUrl("https://xn----7sbbumlywddfdf6a.xn--p1ai/");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+
+        if (MyWebChromeClient.mUploadMessagesAboveL != null) {
+            new WebViewHelper(this, MainActivity.this).onActivityResultAboveL(requestCode, resultCode, intent);
+        }
+
+        if (MyWebChromeClient.mUploadMessage == null) return;
+
+        Uri uri = null;
+
+        if (requestCode == REQUEST_CAMERA && resultCode == RESULT_OK) {
+            uri = cameraUri;
+        }
+
+        if (requestCode == REQUEST_CHOOSE && resultCode == RESULT_OK) {
+            uri = afterChosePic(intent);
+        }
+
+        MyWebChromeClient.mUploadMessage.onReceiveValue(uri);
+        MyWebChromeClient.mUploadMessage = null;
+        super.onActivityResult(requestCode, resultCode, intent);
     }
 }
---
+++ app/src/main/java/com/stdio/drivetaxi/MyWebViewClient.java
@@ -0,0 +1,57 @@
+package com.stdio.drivetaxi;
+
+import android.annotation.TargetApi;
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.util.Log;
+import android.view.View;
+import android.webkit.CookieSyncManager;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import static com.stdio.drivetaxi.MainActivity.TAG;
+
+public class MyWebViewClient extends WebViewClient {
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        return handleUri(view, url);
+    }
+
+    @TargetApi(Build.VERSION_CODES.N)
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+        String url = request.getUrl().toString();
+        return handleUri(view, url);
+    }
+
+    private boolean handleUri(WebView view, String url) {
+        view.loadUrl(url);
+        return true;
+    }
+
+    public MyWebViewClient() {
+        super();
+    }
+
+
+    @Override
+    public void onPageStarted(WebView view, String url, Bitmap favicon) {
+        Log.d(TAG, "URL地址:" + url);
+        MainActivity.progressBar.setVisibility(View.VISIBLE);
+        MainActivity.mWebView.setVisibility(View.GONE);
+        super.onPageStarted(view, url, favicon);
+    }
+
+    @Override
+    public void onPageFinished(WebView view, String url) {
+        Log.i(TAG, "onPageFinished");
+        CookieSyncManager.getInstance().sync();
+        MainActivity.progressBar.setVisibility(View.GONE);
+        MainActivity.mWebView.setVisibility(View.VISIBLE);
+        super.onPageFinished(view, url);
+    }
+
+}
